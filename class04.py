1db2fbc430cf1094ab61e034b98ba1bb7287b13c
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-10-08 15:15:26 +0500

    Adding class04 code

---
+++ class04.py
@@ -0,0 +1,34 @@
+# YOUR VOICE IS NOT EDIBLE
+# about_ali = {
+#     "name": "Ali",
+#     "age": 20,
+#     "father_name" : "Raza"
+# }
+# dic_values = list(about_ali.values())
+# dic_keys = list(about_ali.keys())
+# for i in range(len(about_ali)):
+#     print(f"{dic_keys[i]} : {dic_values[i]}")
+
+
+# for {var_name} == i in range(len(variable_name)): i ---> int() 0,1,2,3,4,5,6
+# for {var_name} == s in variable_name===aboutali.keys(): s ---> [any datatype()] 
+
+# alphabets = [a,b,c,d,e,f,g]
+#              0,1,2,3,4,5,6
+# len() ---> list() [len(alphabets) ---> OUTPUT: 6  --> integer value
+
+dict_with_list = {
+    "school_name" : "Ali-Garh University",
+    "school_founder" : {
+        "name" : "Sir Syed Ahmed Khan",
+        "bof" : "October 17, 1817"
+    },
+    "students_details" : [
+         {
+            "name" : "Ali",
+            "age" :21,
+            "food_love_to_eat" : ["biryani","burger","pizza"],
+        }
+    ],
+}
+# print(f"My school name is {dict_with_list['school_name']} the founder name is {}, and there is the student which name is {} ")
